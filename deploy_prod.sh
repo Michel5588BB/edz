b0db8170f89d79b25bf7861089b0784458666d22
macmini-6 <macmini-6@macmini6.local>
2019-01-07 18:13:53 +0530

    my frist commit

---
+++ deploy_prod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ssh -v root@YOUR_IP_HERE << EOF
+
+echo '1. Updating sources'
+cd /patch/to/your/repository/
+git checkout --force master
+git pull
+
+echo "2. Restart apache"
+sudo apache2ctl graceful
+
+echo 'Done!'
+
+EOF
+
